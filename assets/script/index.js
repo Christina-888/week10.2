3b1ac953b014d3f3615031a85d1c93adc21360e6
Christina-888 <christinabykadorova@gmail.com>
2025-01-19 17:37:08 +0300

    upgrade code

--- assets/script/index.js
+++ assets/script/index.js
@@ -1,10 +1,8 @@
 const calculateTotalPrice = (quantity = 2, price = 15000000) => {
   const result = quantity * price;
   const formattedResult = result.toLocaleString('ru-RU');
-  console.log(formattedResult); 
   return alert(`Стоимость покупки: ${formattedResult} рублей.`);
 }
 
-
-const outcome = document.getElementById('test');
+const outcome = document.getElementById('display');
 outcome.textContent = `Стоимость покупки: ${formattedResult} рублей.`;
